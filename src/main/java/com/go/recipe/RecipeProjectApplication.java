31bb222c8377e2874e8d24c3adc6663384f024a6
codergo7 <codergo7@gmail.com>
2021-05-16 22:43:33 +0200

    Initial commit

---
+++ src/main/java/com/go/recipe/RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.go.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeProjectApplication.class, args);
+    }
+
+}
