67d4fab0e63d447f0051744fec5db2ccbcf5dc9a
codergo7 <codergo7@gmail.com>
2021-05-23 20:27:17 +0200

    added findByDescription method

--- src/main/java/com/go/recipe/controller/IndexController.java
+++ src/main/java/com/go/recipe/controller/IndexController.java
@@ -1,14 +1,31 @@
 package com.go.recipe.controller;
 
 
+import com.go.recipe.model.entities.Category;
+import com.go.recipe.model.entities.UnitOfMeasure;
+import com.go.recipe.repository.CategoryRepository;
+import com.go.recipe.repository.UnitOfMeasureRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is:" + categoryOptional.get().getId());
+        System.out.println("Uom Id is:" + uomOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/com/go/recipe/repository/CategoryRepository.java
+++ src/main/java/com/go/recipe/repository/CategoryRepository.java
@@ -3,5 +3,11 @@ package com.go.recipe.repository;
 import com.go.recipe.model.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
+
 }
