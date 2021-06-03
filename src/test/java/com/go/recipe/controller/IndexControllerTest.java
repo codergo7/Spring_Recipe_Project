c02d662250189379c824b370f2d2b4e0968d1e9c
codergo7 <codergo7@gmail.com>
2021-06-03 13:04:03 +0200

    implemented testing of MockMvc

--- src/test/java/com/go/recipe/controller/IndexControllerTest.java
+++ src/test/java/com/go/recipe/controller/IndexControllerTest.java
@@ -1,17 +1,28 @@
 package com.go.recipe.controller;
 
+import com.go.recipe.model.entities.Recipe;
 import com.go.recipe.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyObject;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 class IndexControllerTest {
 
@@ -29,11 +40,32 @@ class IndexControllerTest {
         indexController = new IndexController(recipeService);
     }
 
+    public void testMockMVC() throws Exception{
+
+        MockMvc mockMvc = standaloneSetup(indexController).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
     @Test
     void getIndexPage() {
-        assertEquals("index",indexController.getIndexPage(model));
 
+        //given
+        List<Recipe> recipes = new ArrayList();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
+
+        //when
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+
+        //then
+        assertEquals("index",indexController.getIndexPage(model));
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anyObject());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture() /*anyObject()*/);
+        List<Recipe> listController = argumentCaptor.getValue();
+        assertEquals(2,listController.size());
     }
 }
