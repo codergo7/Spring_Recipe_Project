5e2df9f564aee7aa2fe61171718dcf8da334c936
codergo7 <codergo7@gmail.com>
2021-06-21 18:08:13 +0200

    added GetMapping annotation

--- src/main/java/com/go/recipe/controller/IndexController.java
+++ src/main/java/com/go/recipe/controller/IndexController.java
@@ -6,19 +6,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping
 public class IndexController {
 
 private final RecipeService recipeService;
 
-    @RequestMapping({"","/","/index"})
+    @GetMapping({"","/","/index"})
     public String getIndexPage(Model model){
         model.addAttribute("recipes", recipeService.getRecipes());
         log.debug("Requested the index page ");
         return "index";
     }
+
 }
