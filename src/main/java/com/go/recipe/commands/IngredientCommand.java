93ade7acdabc593b2fc21583c6742439e9d1eac0
codergo7 <codergo7@gmail.com>
2021-06-22 09:55:10 +0200

    add commands package

---
+++ src/main/java/com/go/recipe/commands/IngredientCommand.java
@@ -0,0 +1,16 @@
+package com.go.recipe.commands;
+
+import lombok.*;
+
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class IngredientCommand {
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    private UnitOfMeasureCommand unitOfMeasure;
+}
---
+++ src/main/java/com/go/recipe/commands/RecipeCommand.java
@@ -0,0 +1,29 @@
+package com.go.recipe.commands;
+
+import com.go.recipe.model.enums.Difficulty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecipeCommand {
+
+    private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    private Set<IngredientCommand> ingredients = new HashSet();
+    private Difficulty difficulty;
+    private NotesCommand notes;
+
+
+}
