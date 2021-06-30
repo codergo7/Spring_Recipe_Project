d2e8c65ac53bc25e252a4e9052c32aa7fa0a14d0
codergo7 <codergo7@gmail.com>
2021-06-30 13:45:14 +0200

    added newRecipe and saveOrUpdate methods

--- src/main/java/com/go/recipe/controller/RecipeController.java
+++ src/main/java/com/go/recipe/controller/RecipeController.java
@@ -1,28 +1,42 @@
 package com.go.recipe.controller;
 
 
+import com.go.recipe.commands.RecipeCommand;
 import com.go.recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-//@RequestMapping
+@RequestMapping("/recipe")
 public class RecipeController {
 
 private final RecipeService recipeService;
 
-    @GetMapping("/recipe/show/{id}")
+    @GetMapping("/show/{id}")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         log.debug("Requested the recipe/show page ");
         return "recipe/show";
+    }
+    @GetMapping("/new")
+    public String newRecipe(Model model){
+
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping()
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+
+       RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
